a99e79b7189d05c5f5d37cc14409ed32ce7eeef8
stefan <137123241+Stef533@users.noreply.github.com>
2024-01-06 19:49:09 +0100

    exe-18 complete

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express from 'express'
+import 'express-async-errors'
+import morgan from 'morgan'
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+  createImage
+} from './controllers/planets.js'
+import {logIn, signUp} from './controllers/users.js'
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination:(req, file, cb) => {
+    cb(null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+})
+
+const upload = multer({storage})
+
+const app = express()
+const port = 3000
+
+app.use('/uploads', express.static('uploads'))
+app.use('/static', express.static('static'))
+
+app.use(morgan('dev'));
+app.use(express.json())
+app.get('/api/planets', getAll)
+app.get('/api/planets/:id', getOneById)
+app.post("/api/planets", create)
+app.put("/api/planets/:id", updateById)
+app.delete("/api/planets/:id", deleteById)
+
+
+app.post('api/planets/:id/image', upload.single('image'), createImage)
+
+app.post('/api/users/login', logIn)
+app.post('/api/users/login', signUp)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost${port}`)
+})
